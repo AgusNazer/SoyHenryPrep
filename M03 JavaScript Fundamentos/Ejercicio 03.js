8581ea7b8dce51c4945a6fe232fb1467ac517748
AgusNazer <agustin.nazer@hotmail.com>
2023-04-13 21:04:08 -0300

    m03 ejercicios

---
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -0,0 +1,78 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*3️⃣ EJERCICIO 03 3️⃣*/
+
+function sonIguales(x, y) {
+   // Retorna true si "x" e "y" son iguales.
+   // De lo contrario, retorna false.
+ if (x === y ){
+   return true;
+ }else{
+   return false;
+ }
+}
+sonIguales(2,2)
+
+function tienenMismaLongitud(str1, str2) {
+   // Retorna true si los dos strings tienen la misma longitud.
+   // De lo contrario, retorna false.
+   if (str1.length === str2.length){
+      return true;
+   }else {
+      return false;
+   }
+}
+tienenMismaLongitud('hola', 'mi nombre es Pedro')
+
+function menosQueNoventa(num) {
+   // Retorna true si el argumento "num" es menor que noventa.
+   // De lo contrario, retorna false.
+   if ( num < 90){
+      return true;
+   }else{
+      return false;
+   }
+}
+menosQueNoventa(80)
+
+function mayorQueCincuenta(num) {
+   // Retorna true si el argumento "num" es mayor que cincuenta.
+   // De lo contrario, retorna false.
+   if ( num > 50){
+      return true;
+   }else{
+      return false;
+   }
+}
+mayorQueCincuenta(60)
+
+function esPar(num) {
+   // Retorna true si "num" es par.
+   // De lo contrario, retorna false.
+   if ( num % 2 == 0){
+      return true;
+   }else{
+      return false;
+   }
+}
+esPar(2)
+
+function esImpar(num) {
+   // Retorna true si "num" es impar.
+   // De lo contrario, retorna false.
+   if ( num % 2 !== 0){
+      return true;
+   }else{
+      return false;
+   }
+}
+esImpar(4)
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   tienenMismaLongitud,
+   sonIguales,
+   menosQueNoventa,
+   mayorQueCincuenta,
+   esPar,
+   esImpar,
+};
